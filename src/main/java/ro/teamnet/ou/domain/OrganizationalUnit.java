cff52a8be7c3aa119f072bffb33832bbc74840dc
Oana.Mihai <oana.mihai@teamnet.ro>
2015-06-17 16:38:40 +0300

    added class to generate javadoc

---
+++ src/main/java/ro/teamnet/ou/domain/OrganizationalUnit.java
@@ -0,0 +1,15 @@
+package ro.teamnet.ou.domain;
+
+public class OrganizationalUnit{
+
+	private Long id;
+	
+	
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
