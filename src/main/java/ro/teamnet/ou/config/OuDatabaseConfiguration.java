b39745615c091e732ffdb121f7e187842822e00f
Viorel Tacliciu <viorel.tacliciu@teamnet.ro>
2015-07-09 11:16:13 +0300

    ou refactoring

--- src/main/java/ro/teamnet/ou/config/OuDatabaseConfiguration.java
+++ src/main/java/ro/teamnet/ou/config/OuDatabaseConfiguration.java
@@ -1,6 +1,5 @@
 package ro.teamnet.ou.config;
 
-import org.neo4j.graphdb.GraphDatabaseService;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,15 +7,11 @@ import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
-import org.springframework.data.neo4j.cross_store.config.CrossStoreNeo4jConfiguration;
-import org.springframework.data.neo4j.rest.SpringRestGraphDatabase;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import ro.teamnet.bootstrap.extend.AppRepositoryFactoryBean;
 import ro.teamnet.bootstrap.plugin.jpa.DefaultPackagesToScanPlugin;
 import ro.teamnet.bootstrap.plugin.jpa.JpaPackagesToScanPlugin;
 import ro.teamnet.neo.config.Neo4jBaseConfiguration;
-
-import java.io.IOException;
 
 @Configuration
 @EnableTransactionManagement
