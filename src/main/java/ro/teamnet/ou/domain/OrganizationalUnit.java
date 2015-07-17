1d0060ac938899b4e79afe0c49a95eb56aa54a26
Cristian D <criss.dumitru.87@gmail.com>
2015-07-17 11:14:35 +0300

    Add view + controller for managing organizational units.

--- src/main/java/ro/teamnet/ou/domain/OrganizationalUnit.java
+++ src/main/java/ro/teamnet/ou/domain/OrganizationalUnit.java
@@ -1,13 +1,12 @@
 package ro.teamnet.ou.domain;
 
 import org.neo4j.graphdb.Direction;
-import org.springframework.data.neo4j.annotation.Fetch;
-import org.springframework.data.neo4j.annotation.GraphId;
-import org.springframework.data.neo4j.annotation.NodeEntity;
-import org.springframework.data.neo4j.annotation.RelatedTo;
+import org.springframework.data.neo4j.annotation.*;
 import ro.teamnet.ou.domain.jpa.OrganizationalUnitFunction;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
 import java.util.Set;
 
 @NodeEntity
@@ -21,7 +20,31 @@ public class OrganizationalUnit{
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
+    @GraphProperty(propertyName = "code", defaultValue = "")
+    @NotNull
+    @Column(name = "CODE", length = 100, unique = true)
+    private String code;
 
+    @Column(name = "DESCRIPTION")
+    @GraphProperty(propertyName = "description", defaultValue = "")
+    private String description;
+
+    @NotNull
+    @Column(name = "VALID_FROM")
+    @GraphProperty(propertyName = "validFrom", defaultValue = "")
+    @Temporal(TemporalType.DATE)
+    protected Date validFrom;
+
+    @NotNull
+    @Column(name = "VALID_TO")
+    @GraphProperty(propertyName = "validTo", defaultValue = "")
+    @Temporal(TemporalType.DATE)
+    protected Date validTo;
+
+    @NotNull
+    @Column(name = "IS_ACTIVE")
+    @GraphProperty(propertyName = "active", defaultValue = "")
+    protected Boolean active;
 
     private Set<OrganizationalUnitFunction> organizationalUnitFunctions;
 
@@ -44,5 +67,21 @@ public class OrganizationalUnit{
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
