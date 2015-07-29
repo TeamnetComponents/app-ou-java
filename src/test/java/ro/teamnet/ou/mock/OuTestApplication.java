737f603942dd19cbc0db51cd59f94a03b8955af2
Oana.Mihai <oana.mihai@teamnet.ro>
2015-07-29 17:20:38 +0300

    repository integration tests setup - work in progress

---
+++ src/test/java/ro/teamnet/ou/mock/OuTestApplication.java
@@ -0,0 +1,77 @@
+package ro.teamnet.ou.mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.MetricFilterAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.MetricRepositoryAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
+import ro.teamnet.bootstrap.config.*;
+import ro.teamnet.bootstrap.config.apidoc.SwaggerConfiguration;
+import ro.teamnet.bootstrap.config.metrics.JHipsterHealthIndicatorConfiguration;
+
+/**
+ * This is a helper Java class that provides an alternative to creating a web.xml.
+ * A main method is also provided and may be used for running the application.
+ */
+@ComponentScan(
+        basePackages = {"ro.teamnet.bootstrap", "ro.teamnet.ou", "ro.teamnet.neo" },
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                value = {/* Components you wish to exclude */
+                         SwaggerConfiguration.class, //WebConfigurer.class, WebSecurityConfiguration.class,
+                        ThymeleafConfiguration.class, MetricsConfiguration.GraphiteRegistry.class, MetricsConfiguration.class,
+                        CacheConfiguration.class, AsyncConfiguration.class, LoggingAspectConfiguration.class,
+                        JHipsterHealthIndicatorConfiguration.class, CloudDatabaseConfiguration.class, LocaleConfiguration.class,
+                }
+        )
+)
+@EnableAutoConfiguration(
+        exclude = {
+                MetricFilterAutoConfiguration.class,
+                MetricRepositoryAutoConfiguration.class,
+                DatabaseConfiguration.class
+        }
+)
+@SpringBootApplication
+public class OuTestApplication {
+    /**
+     * Main method, used to run the application.
+     */
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(OuTestApplication.class);
+        app.setShowBanner(false);
+
+        SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
+
+        // Check if the selected profile has been set as argument.
+        // if not the development profile will be added
+        addDefaultProfile(app, source);
+        addLiquibaseScanPackages();
+        app.run(args);
+    }
+
+    /**
+     * Set a default profile if it has not been set
+     */
+    protected static void addDefaultProfile(SpringApplication app, SimpleCommandLinePropertySource source) {
+        if (!source.containsProperty("spring.profiles.active")) {
+            app.setAdditionalProfiles("test");
+        }
+    }
+
+    /**
+     * Set the liquibases.scan.packages to avoid an exception from ServiceLocator.
+     */
+    protected static void addLiquibaseScanPackages() {
+        System.setProperty("liquibase.scan.packages", "liquibase.change" + "," + "liquibase.database" + "," +
+                "liquibase.parser" + "," + "liquibase.precondition" + "," + "liquibase.datatype" + "," +
+                "liquibase.serializer" + "," + "liquibase.sqlgenerator" + "," + "liquibase.executor" + "," +
+                "liquibase.snapshot" + "," + "liquibase.logging" + "," + "liquibase.diff" + "," +
+                "liquibase.structure" + "," + "liquibase.structurecompare" + "," + "liquibase.lockservice" + "," +
+                "liquibase.ext" + "," + "liquibase.changelog");
+    }
+
+}
---
+++ src/test/java/ro/teamnet/ou/repository/jpa/FunctionRepositoryTest.java
@@ -0,0 +1,42 @@
+package ro.teamnet.ou.repository.jpa;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import ro.teamnet.ou.domain.jpa.Function;
+import ro.teamnet.ou.mock.OuTestApplication;
+
+import javax.inject.Inject;
+import java.util.Date;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = OuTestApplication.class)
+@WebAppConfiguration
+@IntegrationTest
+@ActiveProfiles("test")
+public class FunctionRepositoryTest {
+
+    public static final String TEST_CODE = "TEST";
+
+    @Inject
+    FunctionRepository functionRepository;
+
+    @Test
+    public void createFunctionTest() {
+        Function function = new Function();
+        function.setCode(TEST_CODE);
+        function.setValidFrom(new Date());
+        function.setValidTo(new Date());
+        function.setActive(true);
+        functionRepository.save(function);
+
+        Function byCode = functionRepository.findByCode(TEST_CODE);
+        Assert.assertNotNull(byCode);
+        Assert.assertEquals(function.getId(), byCode.getId());
+    }
+}
