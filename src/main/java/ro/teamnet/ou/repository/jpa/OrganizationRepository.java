cb955f4c413e9bc800e8fae43c0b47347eedf016
ionut.patrascu <ionut.patrascu@enea.com>
2015-07-31 15:34:10 +0300

    Add OrganizationRepository

---
+++ src/main/java/ro/teamnet/ou/repository/jpa/OrganizationRepository.java
@@ -0,0 +1,11 @@
+package ro.teamnet.ou.repository.jpa;
+
+import ro.teamnet.bootstrap.extend.AppRepository;
+import ro.teamnet.ou.domain.Organization;
+
+/**
+ * Created by ionut.patrascu on 31.07.2015.
+ */
+public interface OrganizationRepository extends AppRepository<Organization, Long>{
+
+}
