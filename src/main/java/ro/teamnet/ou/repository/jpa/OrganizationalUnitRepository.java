17131dd2e93cdd588ef0ebb5d2f3149f513c60da
Marian Spoiala <Marian.Spoiala@teamnet.ro>
2015-07-31 15:32:13 +0300

    Added JPA repositories for OrganizationalUnit and Perspective

---
+++ src/main/java/ro/teamnet/ou/repository/jpa/OrganizationalUnitRepository.java
@@ -0,0 +1,10 @@
+package ro.teamnet.ou.repository.jpa;
+
+import ro.teamnet.bootstrap.extend.AppRepository;
+import ro.teamnet.ou.domain.OrganizationalUnit;
+
+/**
+ * Created by Marian.Spoiala on 7/31/2015.
+ */
+public interface OrganizationalUnitRepository extends AppRepository<OrganizationalUnit, Long> {
+}
---
+++ src/main/java/ro/teamnet/ou/repository/jpa/PerspectiveRepository.java
@@ -0,0 +1,9 @@
+package ro.teamnet.ou.repository.jpa;
+
+import ro.teamnet.bootstrap.extend.AppRepository;
+
+/**
+ * Created by Marian.Spoiala on 7/31/2015.
+ */
+public interface PerspectiveRepository extends AppRepository<PerspectiveRepository, Long> {
+}
