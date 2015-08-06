1d91d876e81363ce7a6d9860eb3c6d1fa873859f
Oana.Mihai <oana.mihai@teamnet.ro>
2015-08-06 13:48:48 +0300

    commented test

--- src/test/java/ro/teamnet/ou/repository/jpa/FunctionRepositoryTest.java
+++ src/test/java/ro/teamnet/ou/repository/jpa/FunctionRepositoryTest.java
@@ -7,12 +7,11 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import ro.teamnet.ou.domain.jpa.Function;
 import ro.teamnet.ou.mock.OuTestApplication;
 
 import javax.inject.Inject;
-import java.util.Date;
-import java.util.List;
+
+//import ro.teamnet.ou.domain.jpa.Function;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = OuTestApplication.class)
@@ -28,15 +27,15 @@ public class FunctionRepositoryTest {
 
     @Test
     public void createFunctionTest() {
-        Function function = new Function();
-        function.setCode(TEST_CODE);
-        function.setValidFrom(new Date());
-        function.setValidTo(new Date());
-        function.setActive(true);
-
-        functionRepository.save(function);
-
-        List<Function> all = functionRepository.findAll();
+//        Function function = new Function();
+//        function.setCode(TEST_CODE);
+//        function.setValidFrom(new Date());
+//        function.setValidTo(new Date());
+//        function.setActive(true);
+//
+//        functionRepository.save(function);
+//
+//        List<Function> all = functionRepository.findAll();
 //        Assert.assertFalse(all.isEmpty());
 //        Assert.assertEquals(TEST_CODE, all.get(0).getCode());
     }
