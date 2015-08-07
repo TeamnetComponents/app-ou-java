0cfdfebd9d46ecf10df5536927c980c0950e9133
Oana.Mihai <oana.mihai@teamnet.ro>
2015-08-07 15:09:37 +0300

    fixed neo config

--- src/test/java/ro/teamnet/ou/repository/jpa/FunctionRepositoryTest.java
+++ src/test/java/ro/teamnet/ou/repository/jpa/FunctionRepositoryTest.java
@@ -3,28 +3,19 @@ package ro.teamnet.ou.repository.jpa;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
-import ro.teamnet.ou.TestUtil;
 import ro.teamnet.ou.domain.jpa.Function;
 import ro.teamnet.ou.mock.OuTestApplication;
 import ro.teamnet.ou.service.FunctionService;
-import ro.teamnet.ou.web.rest.FunctionResource;
 
-import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import java.util.Date;
 import java.util.List;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = OuTestApplication.class)
@@ -39,14 +30,6 @@ public class FunctionRepositoryTest {
     private FunctionService functionService;
     @Inject
     private FunctionRepository functionRepository;
-    private MockMvc mockMvc;
-
-    @PostConstruct
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-        FunctionResource functionResource = new FunctionResource(functionService);
-        this.mockMvc = MockMvcBuilders.standaloneSetup(functionResource).build();
-    }
 
     @Test
     @Transactional
@@ -58,16 +41,18 @@ public class FunctionRepositoryTest {
         function.setValidFrom(new Date());
         function.setValidTo(new Date());
         function.setActive(true);
-        mockMvc.perform(post("/app/rest/function")
-                .contentType(TestUtil.APPLICATION_JSON_UTF8)
-                .content(TestUtil.convertObjectToJsonBytes(function)))
-                .andExpect(status().isOk());
+
+        functionService.save(function);
+//        mockMvc.perform(post("/app/rest/function")
+//                .contentType(TestUtil.APPLICATION_JSON_UTF8)
+//                .content(TestUtil.convertObjectToJsonBytes(function)))
+//                .andExpect(status().isOk());
 
         List<Function> all = functionRepository.findAll();
 
         System.out.println(all.size());
-//        Assert.assertFalse(all.isEmpty());
-//        Assert.assertEquals(1, all.size());
-//        Assert.assertEquals(TEST_CODE, all.get(0).getCode());
+        Assert.assertFalse(all.isEmpty());
+        Assert.assertEquals(1, all.size());
+        Assert.assertEquals(TEST_CODE, all.get(0).getCode());
     }
 }
